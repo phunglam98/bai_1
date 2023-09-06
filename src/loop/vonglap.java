31e61d13512dc9858a4a5e5d76dcd4aead06956c
phunglam98 <duclam9812@gmail.com>
2023-09-06 10:08:04 +0700

    bai_1

---
+++ src/loop/vonglap.java
@@ -0,0 +1,74 @@
+package loop;
+
+public class vonglap {
+    public static void main(String[] args) {
+        // hien thi cac loai hinh
+
+//        for (int i = 0; i < 3; i++) {
+//            for (int j = 0; j < 5; j++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println("");
+//        }
+//        System.out.println("");
+//        System.out.println("");
+//        for (int i = 0; i < 5; i++) {
+//            for (int j = 5; j > i; j--) {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//        System.out.println("");
+//        System.out.println("");
+//        for (int i = 0; i < 5; i++) {
+//            for (int j = 0; j < i+1; j++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+        // hien thi 20 so nguyen to dau tien
+//        int count = 1;
+//
+//        System.out.println("2");
+//        for (int i = 3; ; i++) {
+//            boolean SNT = false;
+//            for (int j = 2; j <= Math.sqrt(i); j++) {
+//                if (i % j == 0) {
+//                    SNT = true;
+//                    break;
+//                }
+//            }
+//            if (!SNT) {
+//                count++;
+//                System.out.println(i+" ");
+//            }
+//            if (count > 19) {
+//                break;
+//            }
+//        }
+        // hiển thị các số nguyên tố nhỏ hơn 100
+//        System.out.println("các số nguyên tố nhỏ hơn 100 là: ");
+//        for (int i = 0; i < 100; i++) {
+//            if (snt(i)){
+//                System.out.println(i+"");
+//
+//            }
+//
+//        }
+//
+//    }
+//    private static boolean snt(int n) {
+//        // số nguyên n < 2 không phải là số nguyên tố
+//        if (n < 2) {
+//            return false;
+//        }
+//        // check số nguyên tố khi n>=2
+//        int squareRoot = (int) Math.sqrt(n);
+//        for (int i = 2; i <= squareRoot; i++) {
+//            if (n % i == 0) {
+//                return false;
+//            }
+//        }
+//        return true;
+    }
+}
---
+++ src/overview/democ06.java
@@ -0,0 +1,56 @@
+package overview;
+
+import java.util.Scanner;
+
+public class democ06 {
+    public static void main(String[] args) { //psvm | main + tab
+//        int a;
+//        a = 10;
+//        //<=> int a = 10;
+//        //muốn sử dụng biến trong java thì biến phải đuược khai báo và có giá trị
+//        System.out.println("helo");
+//        System.out.println(a);
+//        int d =10, b = 20, c = 30; //khai báo 3 biến d,b,c có cùng kiểu dữ liệu
+//        //int class không đc khai báo trung key java
+//        //kiểu dữ liệu
+//        //số: int ||| số thập phân: double
+//        // chuỗi: string
+//        // boolean: boolean
+//        //char: kí tự*/
+        // các
+        Scanner input = new Scanner(System.in);
+        Scanner inputString = new Scanner(System.in);
+//        System.out.println("nhập a");
+////        int a = input.nextInt();
+//        String a = input.nextLine(); // cho phép nhập chuỗi
+//        System.out.println(a);
+//
+//        int a = input.nextInt();
+//        System.out.println("nhập a đi: ");
+//        String b = inputString.nextLine();
+//        System.out.println("giá trị là: ");
+//        System.out.println(a);
+//        System.out.println(b);
+
+
+        // nhập điiểm thi in ra điểm giỏi > 90 điểm
+//        System.out.println("nhập điểm: ");
+//        double score = input.nextDouble();
+//        if (score > 90){
+//            System.out.println("giỏi");
+//        } else {
+//            System.out.println("dốt");
+//        }
+//        Scanner inputString = new Scanner(System.in);
+        System.out.println("nhập tên đi: ");
+        String name = inputString.nextLine();
+        switch (name){
+            case "c06":
+
+        }
+
+
+
+
+    }
+}
