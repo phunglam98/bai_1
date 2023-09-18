5f066f1ca05e8ae94d11b8f74b7df661b91d8a0e
phunglam98 <duclam9812@gmail.com>
2023-09-18 18:13:14 +0700

    Abinterface

---
+++ src/Abinterface/RectangleTest.java
@@ -0,0 +1,12 @@
+package Abinterface;
+
+public class RectangleTest {
+    public static void main(String[] args) {
+        Shape shape = new Shape();
+        System.out.println(shape);
+
+        shape = new Shape("red", false);
+        System.out.println(shape);
+    }
+}
+
---
+++ src/Abinterface/Resizeable.java
@@ -0,0 +1,5 @@
+package Abinterface;
+
+public interface Resizeable {
+    double resize();
+}
